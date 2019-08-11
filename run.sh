084b0ba29df3fd2bb1e2d78a2a64d8af34bff62a
Matthew Allen Fisher <11fishem@gmail.com>
2019-08-10 22:49:42 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ python style.py --style examples/content/roda1.jpg \
   --checkpoint-dir checkpoints/ \
   --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
   --train-path /datasets/coco/coco_train2014 \
-  --model-dir /artifacts \
   --test examples/content/face.jpg \
   --test-dir tests/ \
   --content-weight 1.5e1 \
