ed636887cbf5ef17fe8f21409359153ceb140217
Matthew A Fisher <11fishem@gmail.com>
2019-08-12 16:56:19 -0400

    hey

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
-mkdir checkpoints
 mkdir tests
 
 export PYTHONUNBUFFERED=0
-python style.py --style examples/content/roda1.jpg \
-  --checkpoint-dir checkpoints/ \
+python style.py --style examples/content/unnamed.jpg \
+  --checkpoint-dir /artifacts \
   --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
   --train-path /datasets/coco/coco_train2014 \
   --test examples/content/face.jpg \
   --test-dir tests/ \
   --content-weight 1.5e1 \
   --checkpoint-iterations 1000 \
-  --batch-size 20
+  --batch-size 40
+
+python evaluate.py --checkpoint /artifacts \
+--in-path examples/content \
+--out-path /artifacts --allow-different-dimensions
