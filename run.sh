81287033ae418f0e2969dec20d5a0e780e6eb2e1
Matthew A Fisher <11fishem@gmail.com>
2019-08-12 17:02:57 -0400

    updated commands

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 mkdir tests
 
 export PYTHONUNBUFFERED=0
-python style.py --style examples/content/unnamed.jpg \
+python style.py --style examples/style/tsai1.jpg \
   --checkpoint-dir /artifacts \
   --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
   --train-path /datasets/coco/coco_train2014 \
@@ -9,7 +9,7 @@ python style.py --style examples/content/unnamed.jpg \
   --test-dir tests/ \
   --content-weight 1.5e1 \
   --checkpoint-iterations 1000 \
-  --batch-size 40
+  --batch-size 69
 
 python evaluate.py --checkpoint /artifacts \
 --in-path examples/content \
